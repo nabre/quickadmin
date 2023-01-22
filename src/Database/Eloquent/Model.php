bb5169bd950c4dfb005ce974d68bf196ed7f89a3
nabre <nabrejunior@gmail.com>
2023-01-22 06:41:25 +0100

    create

---
+++ src/Database/Eloquent/Model.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nabre\Quickadmin\Database\Eloquent;
+
+use Illuminate\Database\Eloquent\Model as IlluminateModel;
+
+class Model extends IlluminateModel{
+
+}
---
+++ src/Providers/AppServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nabre\Quickadmin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider{
+
+    function boot(){
+
+    }
+
+    function register(){
+
+    }
+}
